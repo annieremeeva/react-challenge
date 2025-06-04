1d46bd5bd73dc768a925444fb7938cfc7e7081b1
annieremeeva <annieremeeva@gmail.com>
2025-06-04 10:40:47 +0300

    reorganized structure of app, finished color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import './styles/index.sass'
-import SimpleCounter from "./components/simpleCounter.jsx";
-import ToDoList from "./components/toDoList/toDoList.jsx";
-import ColorPicker from "./components/colorPicker.jsx";
-import {useRef, useState} from "react";
-import {NavLink, Link, Outlet} from 'react-router-dom';
+import {NavLink, Link, Outlet, useLocation} from 'react-router-dom';
+import {useRef} from "react";
 function App() {
     const puzzles = [
         {
@@ -19,14 +16,56 @@ function App() {
             title: '3. Color Picker',
         },
     ]
-
-
+    const location = useLocation();
+    const currentPath = location.pathname;
+    const index = puzzles.findIndex((item) => item.route === currentPath);
+    const sidebarNav = useRef(null);
+    const bottomNav = useRef(null);
+    const toggleSidebar = () => {
+        sidebarNav.current.classList.toggle('app__nav_sidebar-active');
+    }
     return (
         <div className="app">
-            <nav>
-                { puzzles.map((puzzle, index) => <NavLink key={index} to={puzzle.route}>{puzzle.title}</NavLink>) }
-            </nav>
-            <Outlet/>
+            <div className="app__header">
+                <button className="app__toggle-button" type='button' onClick={toggleSidebar}><span className="app__toggle-lines"></span></button>
+                <h1 className="app__header-text">
+                    React Challenge
+                </h1>
+            </div>
+            <div className="app__body">
+                <div className="app__nav-container_sidebar">
+                    <nav ref={sidebarNav} className="app__nav_sidebar">
+                        { puzzles.map((puzzle, index) => <NavLink onClick={toggleSidebar} key={index} to={puzzle.route} className="app__nav-link_sidebar">{puzzle.title}</NavLink>) }
+                    </nav>
+                </div>
+
+
+                <div className="app__content">
+                    <Outlet/>
+                    <nav ref={bottomNav} className="app__nav_bottom">
+                        {
+                            <NavLink
+                                className="app__nav-link_bottom"
+                                style={{display: `${ index === 0 ? 'none' : 'initial'}`}}
+                                to={`${puzzles[index - 1] ? puzzles[index - 1].route : '/'}`}>
+                                {puzzles[index - 1] ? puzzles[index - 1].title : '/'}
+                            </NavLink>
+
+                        }
+                        {
+                            <NavLink
+                                className="app__nav-link_bottom"
+                                style={{display: `${ index === puzzles.length - 1 ? 'none' : 'initial'}`}}
+                                to={`${puzzles[index + 1] ? puzzles[index + 1].route : '/'}`}>
+                                {puzzles[index + 1] ? puzzles[index + 1].title : '/'}
+                            </NavLink>
+                        }
+                    </nav>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
--- src/components/colorPicker.jsx
+++ src/components/colorPicker.jsx
@@ -1,29 +1,44 @@
 import React, {useState} from 'react';
+import tinycolor from "tinycolor2";
 
 function ColorPicker() {
     const [color, setColor] = useState('#ffffff')
     const initialRecentColors = [];
     const [recentColors, setRecentColors] = useState(initialRecentColors);
+    const [convertedColor, setConvertedColor] = useState('#ffffff')
     const handleRecentColors = () => {
         if (recentColors[0] !== color) {
         setRecentColors( recentColors.length < 10  ? [color, ...recentColors] :[color, ...recentColors].slice(0, -1) );
+        }
+    }
+    const handleConvertion = (color, system) => {
+        let currentColor = tinycolor(color);
+        if (system === "hex") {
+            return (currentColor.toHexString())
+        }
+        if (system === "hsl") {
+            return (currentColor.toHslString())
+        }
+        if (system === "rgb") {
+            return (currentColor.toRgbString())
         }
     }
     return (
         <div className="color-picker">
             <h2>3. Color Picker</h2>
             <div className="color-picker__main-color">
-                <label className="color-picker__main-color">
+                <label className="color-picker__main-color-choice">
                     <input className="color-picker__input" value={color} onBlur={handleRecentColors} onChange={(e) => setColor(e.target.value)} type="color"/>
                     <p>{color}</p>
+                    <p>{handleConvertion(color, 'hsl')}</p>
+                    <p>{handleConvertion(color, 'rgb')}</p>
                 </label>
                 <div className="color-picker__color-swatch" style={{backgroundColor: `${color}`}}></div>
             </div>
             <div className="color-picker__recent-colors">
                 {recentColors.map((color, index) => (
                     <div key={index}>
-                        <div style={{backgroundColor: `${color}`}} className="color-picker__color-swatch"></div>
-                        {/*<p className="color-picker__color-name">{color}</p>*/}
+                        <button style={{backgroundColor: `${color}`}} className="color-picker__color-swatch"></button>
                     </div>
                 ))}
             </div>
